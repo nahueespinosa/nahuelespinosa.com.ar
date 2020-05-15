ed85918e065562143a46186944d513942c86c58d
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-15 09:19:44 -0300

    Change banner and header text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hi there</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>I'm an electronics engineer and software developer<br />
+                passionate about computer science and related topics.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
