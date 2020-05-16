3df509619b9d09ef84cdbe8baf2305881903d523
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-16 15:37:44 -0300

    Fix warning
    'config' is defined but never used  no-unused-vars.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="copyright">
-                <li>&copy;2020, Nahuel Espinosa</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+                <li>{config.copyright}</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
             </ul>
         </div>
     </footer>
