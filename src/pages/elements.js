849325d6f8ab494fab16a1fac80a5a559adca0e9
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-16 15:55:07 -0300

    Fix warning
    The href attribute requires a valid value to be accessible.

--- src/pages/elements.js
+++ src/pages/elements.js
@@ -57,7 +57,7 @@ const Elements = (props) => (
                                 <p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
                                 This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
                                 This is <u>underlined</u> and this is code: <code>for (;;)</code>.
-                                Finally, this is a <a href="#">link</a>.</p>
+                                Finally, this is a <a href="# ">link</a>.</p>
                                 <hr />
                                 <h2>Heading Level 2</h2>
                                 <h3>Heading Level 3</h3>
@@ -98,17 +98,17 @@ const Elements = (props) => (
 
                                         <h4>Icons</h4>
                                         <ul className="icons">
-                                            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                                            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-                                            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-                                            <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                                            <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                                            <li><a href="#" className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
+                                            <li><a href="# " className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                                            <li><a href="# " className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+                                            <li><a href="# " className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                                            <li><a href="# " className="icon fa-github"><span className="label">Github</span></a></li>
+                                            <li><a href="# " className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                                            <li><a href="# " className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
                                         </ul>
                                         <ul className="icons">
-                                            <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                                            <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                                            <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                                            <li><a href="# " className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                                            <li><a href="# " className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                                            <li><a href="# " className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                                         </ul>
 
                                     </div>
@@ -131,36 +131,36 @@ const Elements = (props) => (
 
                                 <h4>Actions</h4>
                                 <ul className="actions">
-                                    <li><a href="#" className="button special">Default</a></li>
-                                    <li><a href="#" className="button">Default</a></li>
+                                    <li><a href="# " className="button special">Default</a></li>
+                                    <li><a href="# " className="button">Default</a></li>
                                 </ul>
                                 <ul className="actions small">
-                                    <li><a href="#" className="button special small">Small</a></li>
-                                    <li><a href="#" className="button small">Small</a></li>
+                                    <li><a href="# " className="button special small">Small</a></li>
+                                    <li><a href="# " className="button small">Small</a></li>
                                 </ul>
                                 <div className="grid-wrapper">
                                     <div className="col-6">
                                         <ul className="actions vertical">
-                                            <li><a href="#" className="button special">Default</a></li>
-                                            <li><a href="#" className="button">Default</a></li>
+                                            <li><a href="# " className="button special">Default</a></li>
+                                            <li><a href="# " className="button">Default</a></li>
                                         </ul>
                                     </div>
                                     <div className="col-6">
                                         <ul className="actions vertical small">
-                                            <li><a href="#" className="button special small">Small</a></li>
-                                            <li><a href="#" className="button small">Small</a></li>
+                                            <li><a href="# " className="button special small">Small</a></li>
+                                            <li><a href="# " className="button small">Small</a></li>
                                         </ul>
                                     </div>
                                     <div className="col-6">
                                         <ul className="actions vertical">
-                                            <li><a href="#" className="button special fit">Default</a></li>
-                                            <li><a href="#" className="button fit">Default</a></li>
+                                            <li><a href="# " className="button special fit">Default</a></li>
+                                            <li><a href="# " className="button fit">Default</a></li>
                                         </ul>
                                     </div>
                                     <div className="col-6">
                                         <ul className="actions vertical small">
-                                            <li><a href="#" className="button special small fit">Small</a></li>
-                                            <li><a href="#" className="button small fit">Small</a></li>
+                                            <li><a href="# " className="button special small fit">Small</a></li>
+                                            <li><a href="# " className="button small fit">Small</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -168,14 +168,14 @@ const Elements = (props) => (
                                 <h4>Pagination</h4>
                                 <ul className="pagination">
                                     <li><span className="button small disabled">Prev</span></li>
-                                    <li><a href="#" className="page active">1</a></li>
-                                    <li><a href="#" className="page">2</a></li>
-                                    <li><a href="#" className="page">3</a></li>
+                                    <li><a href="# " className="page active">1</a></li>
+                                    <li><a href="# " className="page">2</a></li>
+                                    <li><a href="# " className="page">3</a></li>
                                     <li><span>&hellip;</span></li>
-                                    <li><a href="#" className="page">8</a></li>
-                                    <li><a href="#" className="page">9</a></li>
-                                    <li><a href="#" className="page">10</a></li>
-                                    <li><a href="#" className="button small">Next</a></li>
+                                    <li><a href="# " className="page">8</a></li>
+                                    <li><a href="# " className="page">9</a></li>
+                                    <li><a href="# " className="page">10</a></li>
+                                    <li><a href="# " className="button small">Next</a></li>
                                 </ul>
 
                                 <h3>Blockquote</h3>
@@ -280,30 +280,30 @@ const Elements = (props) => (
 
                                 <h3>Buttons</h3>
                                 <ul className="actions">
-                                    <li><a href="#" className="button special">Special</a></li>
-                                    <li><a href="#" className="button">Default</a></li>
+                                    <li><a href="# " className="button special">Special</a></li>
+                                    <li><a href="# " className="button">Default</a></li>
                                 </ul>
                                 <ul className="actions">
-                                    <li><a href="#" className="button big">Big</a></li>
-                                    <li><a href="#" className="button">Default</a></li>
-                                    <li><a href="#" className="button small">Small</a></li>
+                                    <li><a href="# " className="button big">Big</a></li>
+                                    <li><a href="# " className="button">Default</a></li>
+                                    <li><a href="# " className="button small">Small</a></li>
                                 </ul>
                                 <ul className="actions">
-                                    <li><a href="#" className="button special big">Big</a></li>
-                                    <li><a href="#" className="button special">Default</a></li>
-                                    <li><a href="#" className="button special small">Small</a></li>
+                                    <li><a href="# " className="button special big">Big</a></li>
+                                    <li><a href="# " className="button special">Default</a></li>
+                                    <li><a href="# " className="button special small">Small</a></li>
                                 </ul>
                                 <ul className="actions fit">
-                                    <li><a href="#" className="button special fit">Fit</a></li>
-                                    <li><a href="#" className="button fit">Fit</a></li>
+                                    <li><a href="# " className="button special fit">Fit</a></li>
+                                    <li><a href="# " className="button fit">Fit</a></li>
                                 </ul>
                                 <ul className="actions fit small">
-                                    <li><a href="#" className="button special fit small">Fit + Small</a></li>
-                                    <li><a href="#" className="button fit small">Fit + Small</a></li>
+                                    <li><a href="# " className="button special fit small">Fit + Small</a></li>
+                                    <li><a href="# " className="button fit small">Fit + Small</a></li>
                                 </ul>
                                 <ul className="actions">
-                                    <li><a href="#" className="button special icon fa-search">Icon</a></li>
-                                    <li><a href="#" className="button icon fa-download">Icon</a></li>
+                                    <li><a href="# " className="button special icon fa-search">Icon</a></li>
+                                    <li><a href="# " className="button icon fa-download">Icon</a></li>
                                 </ul>
                                 <ul className="actions">
                                     <li><span className="button special disabled">Special</span></li>
