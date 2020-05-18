bc8d1536da213ef70b1f7247ce3e3f1e5e29518f
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-18 15:06:00 -0300

    Fix syntax

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -19,7 +19,7 @@ const ResourcesPage = (props) => (
         <div id="main" className="alt style1">
             <section id="one">
                 <div className="inner">
-                    <p>Here is a list of resources from the internet that I don't think are widely known (some of them may be). I won't spend much time classifying them, there may be articles, entire websites, blogs, forums, tools and basically anything that I think is worth sharing.</p>
+                    <p>Here is a list of resources from the internet that I think are not widely known (some of them may be). I won't spend much time classifying them, there may be articles, entire websites, blogs, forums, tools and basically anything that I think is worth sharing.</p>
                     <p>Since I work as a professor, I found a couple of these looking for good quality learning materials for my students. To be honest, most of them come from random interesting tweets and reddit posts that come my way.</p>
                 </div>
             </section>
