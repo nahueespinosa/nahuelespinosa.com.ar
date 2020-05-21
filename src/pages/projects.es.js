2459ad6b0c3e083527669f728f6bb40e9819b487
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-21 15:57:54 -0300

    Add graphql query to projects page

--- src/pages/projects.es.js
+++ src/pages/projects.es.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
 import SEO from '../components/seo.js'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import project01 from '../assets/images/project01.jpg'
-import project02 from '../assets/images/project02.jpg'
-import project03 from '../assets/images/project03.jpg'
-import project04 from '../assets/images/project04.jpg'
-import project05 from '../assets/images/project05.jpg'
-import project06 from '../assets/images/project06.gif'
-import project07 from '../assets/images/project07.jpg'
-
-const ProjectsPage = (props) => (
+const ProjectsPage = ({data}) => (
     <Layout lang="es">
         <SEO title="Nahuel Espinosa | Proyectos" lang="es" />
 
@@ -29,143 +22,31 @@ const ProjectsPage = (props) => (
             </section>
             <section id="two">
                 <div className="inner">
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Mi sitio web personal</h3>
-                            </header>
-                            <p>
-                                <span className="image left"><img src={project07} alt="" /></span>Esta página fue desarrollada con Gatsby framework (React, node.js) usando <a href="https://github.com/codebushi/gatsby-starter-forty">este starter</a>. <br/>
-                                Adapté el tema a mis necesidades y agregué/modifiqué varios componentes (<Link to="/es/timeline#two">Timeline</Link>, Banner).
-                            </p>
-                            <ul className="actions">
-                                <li><a href="https://github.com/nahueespinosa/my-site" className="button icon fa-github">Código Fuente</a></li>
-                            </ul>
-                            <ul className="actions">
-                                <li><code>Javascript</code></li>
-                                <li><code>node.js</code></li>
-                                <li><code>React</code></li>
-                                <li><code>Gatsby</code></li>
-                                <li><code>HTML</code></li>
-                                <li><code>CSS</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Visual graph search</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project06} alt="" />
-                            </span>
-                            <div>
-                                <p>Este es un juego que permite visualizar conceptos y jugar con algoritmos de búsqueda para estructuras de datos con grafos. La interfaz está implementada con PyGame, y permite dibujar muros y seleccionar las posiciones iniciales y finales.</p>
-                                <p>Comencé este proyecto después de finalizar el curso <a href="https://github.com/nahueespinosa/ai50">Introduction to Artificial Intelligence</a> de <a href="https://cs50.harvard.edu/ai/2020/">CS50</a>.</p>
-                                <ul className="actions">
-                                    <li><a href="https://github.com/nahueespinosa/visual_graph_search" className="button icon fa-github">Código Fuente</a></li>
-                                </ul>
-                                <ul className="actions">
-                                    <li><code>Python</code></li>
-                                    <li><code>PyGame</code></li>
-                                </ul>
+                    {data.allProjectsEsJson.nodes.map((project) => (
+                        <section>
+                            <div className="content box">
+                                <header className="major">
+                                <h3>{project.title}</h3>
+                                </header>
+                                <span>
+                                    <Image
+                                        fluid={project.image.childImageSharp.fluid}
+                                        alt={project.title}
+                                        style={{ float: "right", width: 250, margin: "0 2rem 2rem" }}
+                                    />
+                                </span>
+                                <div dangerouslySetInnerHTML={ {__html: project.html } }/>
+                                {project.source && <ul className="actions">
+                                    <li><a href={project.source} className="button icon fa-github">Source Code</a></li>
+                                </ul>}
+                                {project.tags && <ul className="actions">
+                                    {project.tags.map( item => (
+                                        <li><code>{item}</code></li>
+                                    ))}
+                                </ul>}
                             </div>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>sAPI port para placa Blue Pill</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project05} alt="" />
-                            </span>
-                            <p>Port de la librería sAPI a la <a href="https://stm32-base.org/boards/STM32F103C8T6-Blue-Pill.html">placa STM32F103C8 Blue Pill</a>. Funciona como una HAL (Capa de Abstracción de Hardware) para microcontroladores usada en el <a href="http://www.proyecto-ciaa.com.ar/">proyecto CIAA</a>.</p>
-                            <p>Proporciona un buen punto de entrada para estudiantes al mundo de los sistemas embebidos.</p>
-                            <ul className="actions">
-                                <li><a href="https://github.com/nahueespinosa/sapi_bluepill" className="button icon fa-github">Código Fuente</a></li>
-                            </ul>
-                            <ul className="actions">
-                                <li><code>ST Microcontrollers</code></li>
-                                <li><code>C</code></li>
-                                <li><code>STM32CubeIDE</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Placa base para STM32F4DISCOVERY</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project04} alt="" />
-                            </span>
-                            <p>Diseño de PCB de una placa base para el <a href="https://www.st.com/en/evaluation-tools/stm32f4discovery.html">kit de desarrollo STM32F4DISCOVERY</a>. Incluye los siguientes periféricos: potenciómetro, teclado, display LCD, encoder, LED RGB, buzzer piezoeléctrico, salidas aisladas (optoacoplador), zócalo micro SD, ESP8266, entradas analógicas 4-20mA.</p>
-                            <ul className="actions">
-                                <li><code>Proteus Design Suite</code></li>
-                                <li><code>ST Microcontrollers</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Medidor de energía</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project03} alt="" />
-                            </span>
-                            <p>Diseño y desarrollo de un dispositivo capaz de medir parámetros eléctricos como potencia activa, tensión y corriente instantáneas, factor de potencia y energía.</p>
-                            <p>Posee almacenamiento interno y un servidor web integrado para acceder a la información en tiempo real. Los principales chips usados fueron LPC1769, ESP8266, ATM90E36A. Conectividad: Ethernet, WiFi, Modbus.</p>
-                            <ul className="actions">
-                                <li><code>LPCOpen</code></li>
-                                <li><code>C</code></li>
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>uIP</code></li>
-                                <li><code>Modbus</code></li>
-                                <li><code>Altium Designer</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Sistema de asiento de vehículo</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project02} alt="" />
-                            </span>
-                            <p>Implementación de un sistema de control de asiento de un vehículo con sensor de temperatura, calentador y sensor de presencia. Dos asientos conectados a través de un bus LIN, y comunicación CAN con el resto del vehículo.</p>
-                            <p>Para la comunicación LIN se utilizó la UART2 del microcontrolador con un transceiver <a href="http://www.ti.com/lit/ds/symlink/tpic1021.pdf?&ts=1589836790029">TPIC1021DR</a>. Y para el bus CAN se utilizó el transceiver <a href="https://www.ti.com/product/SN65HVD1040">SN65HVD1040</a>.</p>
-                            <ul className="actions">
-                                <li><code>CMSIS</code></li>
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>C</code></li>
-                                <li><code>LIN</code></li>
-                                <li><code>CAN</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Sistema de localización de vehículos</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project01} alt="" />
-                            </span>
-                            <p>Diseño y desarrollo de un rastreador cuya función principal es localizar y mostrar información de múltiples vehículos. Usamos un módulo <a href="https://www.itead.cc/sim908-gsm-gprs-gps-module.html">SIM908</a> con una interfaz de comandos AT para enviar la posición GPS sobre GPRS a una aplicación backend.</p>
-                            <p>La aplicación recibe los datos de varios dispositivos y genera un mapa con geocercas configurables. Se almacena todo en una base de datos SQLite para un posterior análisis.</p>
-                            <ul className="actions">
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>GPRS/GPS</code></li>
-                                <li><code>C</code></li>
-                                <li><code>C++</code></li>
-                                <li><code>QtCreator</code></li>
-                                <li><code>SQL</code></li>
-                            </ul>
-                        </div>
-                    </section>
+                        </section>
+                    ))}
                 </div>
             </section>
             <section id="three">
@@ -177,5 +58,26 @@ const ProjectsPage = (props) => (
 
     </Layout>
 )
+
+export const query = graphql`
+    query ProjectsEsQuery {
+        allProjectsEsJson {
+            nodes {
+                tags
+                title
+                html
+                source
+                tags
+                image {
+                    childImageSharp {
+                        fluid {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default ProjectsPage
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
 import SEO from '../components/seo.js'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import project01 from '../assets/images/project01.jpg'
-import project02 from '../assets/images/project02.jpg'
-import project03 from '../assets/images/project03.jpg'
-import project04 from '../assets/images/project04.jpg'
-import project05 from '../assets/images/project05.jpg'
-import project06 from '../assets/images/project06.gif'
-import project07 from '../assets/images/project07.jpg'
-
-const ProjectsPage = (props) => (
+const ProjectsPage = ({data}) => (
     <Layout lang="en">
         <SEO title="Nahuel Espinosa | Projects" />
 
@@ -29,143 +22,31 @@ const ProjectsPage = (props) => (
             </section>
             <section id="two">
                 <div className="inner">
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>My personal website</h3>
-                            </header>
-                            <p>
-                                <span className="image left"><img src={project07} alt="" /></span>This webpage was developed with Gatsby framework (React, node.js) using <a href="https://github.com/codebushi/gatsby-starter-forty">this starter</a>. <br/>
-                                I adapted the theme to my needs and added/modified various componentes (<Link to="/timeline#two">Timeline</Link>, Banner).
-                            </p>
-                            <ul className="actions">
-                                <li><a href="https://github.com/nahueespinosa/my-site" className="button icon fa-github">Source Code</a></li>
-                            </ul>
-                            <ul className="actions">
-                                <li><code>Javascript</code></li>
-                                <li><code>node.js</code></li>
-                                <li><code>React</code></li>
-                                <li><code>Gatsby</code></li>
-                                <li><code>HTML</code></li>
-                                <li><code>CSS</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Visual graph search</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project06} alt="" />
-                            </span>
-                            <div>
-                                <p>This is a game that allows you to visualize concepts and play with search algorithms for graph data structures. The interface is implemented with PyGame, and allows you to draw walls and select the start and end positions.</p>
-                                <p>I started this project after finishing the course <a href="https://github.com/nahueespinosa/ai50">Introduction to Artificial Intelligence</a> from <a href="https://cs50.harvard.edu/ai/2020/">CS50</a>.</p>
-                                <ul className="actions">
-                                    <li><a href="https://github.com/nahueespinosa/visual_graph_search" className="button icon fa-github">Source Code</a></li>
-                                </ul>
-                                <ul className="actions">
-                                    <li><code>Python</code></li>
-                                    <li><code>PyGame</code></li>
-                                </ul>
+                    {data.allProjectsJson.nodes.map((project) => (
+                        <section>
+                            <div className="content box">
+                                <header className="major">
+                                <h3>{project.title}</h3>
+                                </header>
+                                <span>
+                                    <Image
+                                        fluid={project.image.childImageSharp.fluid}
+                                        alt={project.title}
+                                        style={{ float: "right", width: 250, margin: "0 2rem 2rem" }}
+                                    />
+                                </span>
+                                <div dangerouslySetInnerHTML={ {__html: project.html } }/>
+                                {project.source && <ul className="actions">
+                                    <li><a href={project.source} className="button icon fa-github">Source Code</a></li>
+                                </ul>}
+                                {project.tags && <ul className="actions">
+                                    {project.tags.map( item => (
+                                        <li><code>{item}</code></li>
+                                    ))}
+                                </ul>}
                             </div>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>sAPI port for Blue Pill board</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project05} alt="" />
-                            </span>
-                            <p>Port sAPI library to <a href="https://stm32-base.org/boards/STM32F103C8T6-Blue-Pill.html">STM32F103C8 Blue Pill board</a>. It works as a HAL (Hardware Abstraction Layer) for microcontrollers used in the <a href="http://www.proyecto-ciaa.com.ar/">project CIAA</a>.</p>
-                            <p>It provides a good entry point for high school students to approach the embedded systems world.</p>
-                            <ul className="actions">
-                                <li><a href="https://github.com/nahueespinosa/sapi_bluepill" className="button icon fa-github">Source Code</a></li>
-                            </ul>
-                            <ul className="actions">
-                                <li><code>ST Microcontrollers</code></li>
-                                <li><code>C</code></li>
-                                <li><code>STM32CubeIDE</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Baseboard for STM32F4DISCOVERY</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project04} alt="" />
-                            </span>
-                            <p>PCB design of a baseboard for <a href="https://www.st.com/en/evaluation-tools/stm32f4discovery.html">STM32F4DISCOVERY development kit</a>. It includes the following peripherals: potentiometer, keyboard, LCD display, encoder, RGB LED, piezoelectric buzzer, isolated outputs (optocoupler), micro SD socket, ESP8266, 4-20mA analog inputs.</p>
-                            <ul className="actions">
-                                <li><code>Proteus Design Suite</code></li>
-                                <li><code>ST Microcontrollers</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Energy meter</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project03} alt="" />
-                            </span>
-                            <p>Design and development of a device capable of measuring electric parameters such as active power, instant voltage and current, power factor and energy.</p>
-                            <p>It has internal storage and an integrated web server to access the information in real time. The main chips used were LPC1769, ESP8266 and ATM90E36A. Connectivity: Ethernet, WiFi, Modbus.</p>
-                            <ul className="actions">
-                                <li><code>LPCOpen</code></li>
-                                <li><code>C</code></li>
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>uIP</code></li>
-                                <li><code>Modbus</code></li>
-                                <li><code>Altium Designer</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Vehicle seat control system</h3>
-                            </header>
-                            <span className="image right">
-                                <img src={project02} alt="" />
-                            </span>
-                            <p>Implementation of vehicle seat control system with temepature sensor, heater y presence detector. Two seats connected over a LIN bus, and CAN bus available to communicate with the rest of the vehicle.</p>
-                            <p>For the LIN communication we used the UART2 peripheral of the microcontroller with a <a href="http://www.ti.com/lit/ds/symlink/tpic1021.pdf?&ts=1589836790029">TPIC1021DR</a> transceiver. And for the CAN bus we used the <a href="https://www.ti.com/product/SN65HVD1040">SN65HVD1040</a> transceiver.</p>
-                            <ul className="actions">
-                                <li><code>CMSIS</code></li>
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>C</code></li>
-                                <li><code>LIN</code></li>
-                                <li><code>CAN</code></li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="content box">
-                            <header className="major">
-                                <h3>Vehicle localization system</h3>
-                            </header>
-                            <span className="image left">
-                                <img src={project01} alt="" />
-                            </span>
-                            <p>Design and development of a tracker whose main function is to locate and display information from multiple vehicles. We used a <a href="https://www.itead.cc/sim908-gsm-gprs-gps-module.html">SIM908</a> module with AT commands interface to send the GPS location over GPRS to a backend application.</p>
-                            <p>The application receives the data from various devices and generates a map with configurable geofences. Everything is stored in a SQLite database for further analysis.</p>
-                            <ul className="actions">
-                                <li><code>FreeRTOS</code></li>
-                                <li><code>GPRS/GPS</code></li>
-                                <li><code>C</code></li>
-                                <li><code>C++</code></li>
-                                <li><code>QtCreator</code></li>
-                                <li><code>SQL</code></li>
-                            </ul>
-                        </div>
-                    </section>
+                        </section>
+                    ))}
                 </div>
             </section>
             <section id="three">
@@ -177,5 +58,26 @@ const ProjectsPage = (props) => (
 
     </Layout>
 )
+
+export const query = graphql`
+    query ProjectsQuery {
+        allProjectsJson {
+            nodes {
+                tags
+                title
+                html
+                source
+                tags
+                image {
+                    childImageSharp {
+                        fluid {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default ProjectsPage
