6aaae5c6d035ba375a28551d3c264b1597ec1539
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-12-24 15:56:02 -0300

    Update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 const HomeIndex = () => (
   <Layout>
     <h1>Hi there!</h1>
-    <p>I'm Nahuel, I'm an electronics engineer and software developer.</p>
-    <p>There is not much to do here for now.<br/>Feel free to look around though.</p>
+    <p>I'm Nahuel, I'm an electronic engineer and software developer.</p>
+    <p>Not much to do here for now.<br/>Feel free to look around though.</p>
     <h2>Website To-Do List</h2>
     <p><input type="checkbox" disabled="disabled"/> Add night mode, obviously.</p>
   </Layout>
