8247f2a47b508f2ec8b43bb967152f197e8bc4f9
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-12-25 21:36:31 -0300

    Use config title

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Style from "./header.module.css"
 import Toggle from "../components/toggle.js"
+import Config from '../../data/SiteConfig'
 
 const Header = () => (
   <div id="header" className={Style.header}>
     <div className={Style.container}>
-      <span>Nahuel Espinosa</span>
+      <span>{Config.siteTitle}</span>
       <Toggle/>
     </div>
   </div>
