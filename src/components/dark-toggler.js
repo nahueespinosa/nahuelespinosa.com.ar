78ccde0bbf938e97811a803011ba316633d711c5
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-01-01 04:50:55 -0300

    Remove animation

--- src/components/dark-toggler.js
+++ src/components/dark-toggler.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import styled, { css } from "styled-components"
+import React from "react"
+import styled from "styled-components"
 import Switch from "react-switch"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
@@ -13,40 +13,7 @@ const Span = styled.span`
   white-space: nowrap;
 `
 
-const ScaleAnimation = css`
-  @keyframes scale {
-    from {
-      transform: scale(.5, .5);
-      z-index: 10;
-      opacity: 0.5
-    }
-    to {
-      transform: scale(30, 30);
-      opacity: 1;
-    }
-  }
-
- animation: scale var(--theme-animation-time) cubic-bezier(.36, .11, .89, .32);
-`
-
-const Circle = styled.div`
-  border-radius: 50%;
-  background-color: var(--background-color);
-  width: 150px;
-  height: 150px;
-  position: absolute;
-  opacity: 0;
-  z-index: -1;
-
-  ${props => (
-    props.className === 'light' ||
-    props.className === 'dark'
-  ) && ScaleAnimation}
-`
-
 const DarkToggler = ({ label }) => {
-  const [expand, setExpand] = useState(0)
-
   return (
     <ThemeToggler>
       {({ theme, toggleTheme }) => {
@@ -58,13 +25,10 @@ const DarkToggler = ({ label }) => {
           }
           return (
             <Div>
-              <Circle className={expand}/>
               <Span>{label}</Span>
               <Switch 
                 onChange={() => {
-                  const newTheme = (theme === 'dark') ? 'light' : 'dark'
-                  toggleTheme(newTheme)
-                  setExpand(newTheme)
+                  toggleTheme(theme === 'dark' ? 'light' : 'dark')
                 }}
                 checked={theme === 'dark'}
                 height={20}
