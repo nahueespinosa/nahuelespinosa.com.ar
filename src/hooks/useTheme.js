cb4992f86c702e76d04c2da2e7c5e8e9bbbe812a
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-01-03 14:51:52 -0300

    Fix theme load on refresh

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react"
 
 /**
- * A hook to get and update the current theme for dark mode
+ * A hook to get and update the current theme for dark mode.
  * @returns [theme, toggleTheme] - [current theme, function to toggle theme]
  */
 export const useTheme = () => {
   const storedTheme = window !== undefined && window.localStorage.getItem("theme")
   const [ theme, setTheme ] = useState(storedTheme || "light")
-  const toggleTheme = () => {
-    if (theme !== "dark") {
-      document.body.classList.add("dark")
-    } else {
-      document.body.classList.remove("dark")
-    }
 
-    setTheme(prevTheme => {
-      return prevTheme === "light" ? "dark" : "light"
-    })
+  loadTheme(theme)
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light"
+
+    // Set state variable
+    setTheme(() => (newTheme))
+
+    // Load theme
+    loadTheme(newTheme)
   }
 
   useEffect(() => {
@@ -26,4 +27,16 @@ export const useTheme = () => {
   }, [theme])
 
   return [theme, toggleTheme]
+}
+
+/**
+ * A helper function to load the selected theme.
+ * @param {*} theme - Theme to load
+ */
+const loadTheme = (theme) => {
+  if (theme === "dark") {
+    document.body.classList.add("dark")
+  } else {
+    document.body.classList.remove("dark")
+  }
 }
