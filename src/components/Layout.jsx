f50882a33056cb21432d18cce86117be79bcc92d
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-01-03 11:54:23 -0300

    Remove gatsby-plugin-dark-mode

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useIntl, FormattedMessage } from "gatsby-plugin-intl"
-import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import { FaMapMarkerAlt, FaRegMoon } from "react-icons/fa"
 import { ImSun } from "react-icons/im"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
@@ -17,51 +16,36 @@ const Layout = ({ children }) => {
   const { title, location, social } =  useSiteMetadata()
   
   return (
-    <ThemeToggler>
-      {({ theme, toggleTheme }) => {
-        // Don't render anything at compile time. Deferring rendering until we
-        // know which theme to use on the client avoids incorrect initial
-        // state being displayed.
-        if (theme == null) {
-          return null
-        }
+    <>
+      <Header title={title}>
+        <LanguageButton label={t("Layout.Language")} />
+        <Toggler
+          iconChecked={<FaRegMoon/>}
+          iconUnchecked={<ImSun/>}
+          onChange={() => {}}
+          checked={false}
+        />
+      </Header>
 
-        return (
-          <>
-            <Header title={title}>
-              <LanguageButton label={t("Layout.Language")} />
-              <Toggler
-                iconChecked={<FaRegMoon/>}
-                iconUnchecked={<ImSun/>}
-                onChange={() => {
-                  toggleTheme(theme === 'dark' ? 'light' : 'dark')
-                }}
-                checked={theme === 'dark'}
-              />
-            </Header>
+      <Main>
+        {children}
+      </Main>
 
-            <Main>
-              {children}
-            </Main>
-
-            <Footer
-              social={social}
-              message={
-                <p>
-                  <FormattedMessage
-                    id="Layout.Contact"
-                    values={{a: chunks => (<a href={social.email}>{chunks}</a>)}}
-                  />
-                  <br/>
-                  {t("Layout.Location")} <FaMapMarkerAlt/>&nbsp;
-                  <a href={"https://www.google.com.ar/maps/place/" + location}>{location}</a>.
-                </p>
-              }
+      <Footer
+        social={social}
+        message={
+          <p>
+            <FormattedMessage
+              id="Layout.Contact"
+              values={{a: chunks => (<a href={social.email}>{chunks}</a>)}}
             />
-          </>
-        )
-      }}
-    </ThemeToggler>
+            <br/>
+            {t("Layout.Location")} <FaMapMarkerAlt/>&nbsp;
+            <a href={"https://www.google.com.ar/maps/place/" + location}>{location}</a>.
+          </p>
+        }
+      />
+    </>
   )
 }
 
