9060d885a8df9d6d752dfb0a3cbd928b466afe3b
Nahuel Espinosa <nahue.espinosa@gmail.com>
2021-01-03 14:24:02 -0300

    Move ThemeProvider to wrapRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,12 @@
+import React from "react"
+import { ThemeProvider } from "./src/components/ThemeProvider"
+
 require("./src/styles/global.css")
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider>
+      {element}
+    </ThemeProvider>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { useIntl, FormattedMessage } from "gatsby-plugin-intl"
 import { FaMapMarkerAlt, FaRegMoon } from "react-icons/fa"
 import { ImSun } from "react-icons/im"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
-import { ThemeProvider, ThemeConsumer } from './ThemeProvider'
+import { ThemeConsumer } from './ThemeProvider'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
   const { title, location, social } =  useSiteMetadata()
   
   return (
-    <ThemeProvider>
+    <>
       <Header title={title}>
         <LanguageButton label={t("Layout.Language")} />
         <ThemeConsumer>
@@ -50,7 +50,7 @@ const Layout = ({ children }) => {
           </p>
         }
       />
-    </ThemeProvider>
+    </>
   )
 }
 
